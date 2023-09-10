aa4ef6c126038cccaeac9b578fd8a902aff37f73
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2023-09-10 16:18:15 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -99,4 +99,6 @@ CONFIG_PACKAGE_luci-app-nlbwmon=n
 CONFIG_PACKAGE_coremark=n
 CONFIG_PACKAGE_luci-app-autoreboot=n
 CONFIG_PACKAGE_luci-i18n-autoreboot-zh-cn=n
+CONFIG_PACKAGE_wol=n
+CONFIG_PACKAGE_etherwake=n
 EOF
