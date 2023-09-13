b0002c90ee53e5c34ebd5d07a5efbb7616adef3b
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2023-09-13 14:30:08 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,14 +17,3 @@ cd openwrt
 
 #Add amlogic管理
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/lean/luci-app-amlogic
-
-
-
-#Add openclash
-# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/lean/luci-app-openclash
-
-#Add luci-app-smartdns
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
-
-#Add luci-app-adguardhome
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
