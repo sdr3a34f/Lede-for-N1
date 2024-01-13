c60164280e4c2ba1e8cbaf1a9da88e542f852adc
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2024-01-13 19:07:22 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,9 +16,18 @@ cd openwrt
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 #Add amlogic管理
-# svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/lean/luci-app-amlogic
-# svn_export "main" "luci-app-amlogic" "package/luci-app-amlogic" "https://github.com/ophub/luci-app-amlogic"
-# rm -rf package/luci-app-amlogic
-# git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 
-svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
+svn_export() {
+	# 参数1是分支名, 参数2是子目录, 参数3是目标目录, 参数4仓库地址
+	TMP_DIR="$(mktemp -d)" || exit 1
+ 	ORI_DIR="$PWD"
+	[ -d "$3" ] || mkdir -p "$3"
+	TGT_DIR="$(cd "$3"; pwd)"
+	git clone --depth 1 -b "$1" "$4" "$TMP_DIR" >/dev/null 2>&1 && \
+	cd "$TMP_DIR/$2" && rm -rf .git >/dev/null 2>&1 && \
+	cp -a . "$TGT_DIR/" && cd "$ORI_DIR"
+	rm -rf "$TMP_DIR"
+}
+
+svn_export "main" "luci-app-amlogic" "package/luci-app-amlogic" "https://github.com/ophub/luci-app-amlogic"
+
