dc0a6203a5e2523a2c10ce8ddb471e2981b1f488
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2024-01-27 09:08:53 +0800

    Update src.sh

--- src.sh
+++ src.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd openwrt
+
 # Add a feed source
 # echo 'src-git SSRplus https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
@@ -9,4 +9,4 @@ echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-pa
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 
 # 修复编译错误
-# sed -i 's/TARGET_CFLAGS += -DHAVE_MAP_SYNC/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g'  feeds/packages/utils/xfsprogs/Makefile
+sed -i 's/TARGET_CFLAGS += -DHAVE_MAP_SYNC/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g'  feeds/packages/utils/xfsprogs/Makefile
