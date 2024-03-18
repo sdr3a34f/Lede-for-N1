355c826238ef5e884b12ce3a2ace2b1d5ad725e8
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2024-03-18 12:57:44 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 cd openwrt
 
-# OpenWrt golang新版本
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-
 #Add amlogic管理
 git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
 mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
