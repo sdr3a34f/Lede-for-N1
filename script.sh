b47c57a3846c8300e67fa8bd15f0ce192fc9b559
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2024-03-18 09:04:41 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-
-# wireless
-#rm -rf files/etc/config/wireless
-#rm -rf files/etc/modules.d/wireless_enable
-# Firewall custom
-#echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
-
-# geodata
-# wget -q -cP files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-# wget -q -cP files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-
-# Add luci-theme
-# rm -rf package/lean/luci-theme-argon
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+cd openwrt
 
 #Add amlogic管理
 git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
