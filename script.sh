216092c4f439787f745dd9a33302ab1ae963e4fb
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2024-04-03 12:59:31 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,4 +7,5 @@ mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
 rm -rf package-temp
 
 ## 微信/Telegram 推送插件
-git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git
+rm -rf feeds/luci/applications/luci-app-wechatpush
+git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git feeds/luci/applications/luci-app-wechatpush
